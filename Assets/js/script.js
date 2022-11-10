caa2e4bca87d30f91aa4f696c63aa97958831b41
JoeBosz <joeboz2213@gmail.com>
2022-11-10 13:31:24 -0600

    Add questions and answers

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,8 +8,33 @@ var time = 20;
 //create A variable for all the questions & choices //hint: array & objects
 //var index = 0
 
+var arr = [
+  {
+    question: "What is 21 x 4?",
+    choices: ['84, 72, 96, 82'],
+    correct: '84',
+
+    question: "What is 1240 ÷ 4",
+    choices: ['320, 310, 360, 310'],
+    correct: '310',
+
+    question: "What is 8 x 9?",
+    choices: ['82, 96, 72, 84'],
+    correct: '72',
+
+    question: "What is 96 ÷ 8?",
+    choices: ['13, 18, 16, 12'],
+    correct: '12',
+
+
+}
+]
+
 //function to display question
 
+function displayQuestion = () {
+  
+}
 //increase index value after user picked a choice
 //call the function to display question again
 
